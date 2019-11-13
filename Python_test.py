9b8b9ff38834e17217d2a51d75160400af220764
da-basset <56661867+da-basset@users.noreply.github.com>
2019-11-13 10:47:49 -0500

    First attempt at simple script

---
+++ Python_test.py
@@ -0,0 +1,7 @@
+# Test for python program that asks for a number between 1 - 200 and replies telling the user if the number is even or odd.
+
+text = print(input("Please enter a number between 1 and 200."))
+if text == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
